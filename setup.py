62997b3c712a6c5eeb51c8d7335327734ea931fc
Gregory Armer <gregory.armer@dealertrack.com>
2014-10-17 10:36:38 -0400

    Bump version for dependency fix

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='djangorest-alchemy',
-    version='0.1.2',
+    version='0.1.3',
     description='Django REST Framework and SQLAlchemy integration',
     long_description=readme + '\n\n' + history,
     author='Ashish Gore',
